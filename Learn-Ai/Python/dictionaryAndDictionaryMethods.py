043279417ec1a35b8bc2b9441d3565238b675282
Envixy <147620166+meetshah1205@users.noreply.github.com>
2024-07-07 13:25:24 +0530

    Delete Learn-Ai/Python directory

--- Learn-Ai/Python/dictionaryAndDictionaryMethods.py
+++
@@ -1,8 +0,0 @@
-amILazy = False
-butSHouldYouJustReadThePythonDocsToKnowAboutAllThis = True
-
-class YouHaveASkillIssueToJustCopyPasteCode(NotImplementedError):
-    pass
-
-if not amILazy and butSHouldYouJustReadThePythonDocsToKnowAboutAllThis:
-    raise YouHaveASkillIssueToJustCopyPasteCode("Don't just copy paste code, make sure you understand and read the code, or even better, just read the documentation and write your own code. I could've just hacked you by running some .exe file.")
